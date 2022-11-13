68f4d1efd2227d30d64fa78fb77b7cee11666953
RajmanCHesus <114088685+RajmanCHesus@users.noreply.github.com>
2022-11-13 11:14:30 +0100

    Update and rename galeria.html to galeria.php

---
+++ ecommerceLab/galeria.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Domov | E-commerce lab</title>
+  <?php
+    include('./head.php');
+    ?>
+</head>
+<body>
+  <!-- horné menu -->
+  <?php
+    include('./header.php');
+    ?>
+  <div class="gallery-container">
+    <div class="gallery-row1">
+      <div class="gallery-left1">
+        <div class="gallery-upper-medium marginbot10 relative contentpic">
+          <img class="gallery-medium-pic gallery"
+            src="https://github.com/snickers-c/commercesurf/blob/wix/wixcopy/media/DSC_0009.jpg?raw=true">
+          <div class="pictext">E-commerce Hálova priestory</div>
+        </div>
+        <div class="gallery-lower-small contentpic">
+          <img class="gallery-small-pic gallery"
+            src="https://github.com/snickers-c/commercesurf/blob/wix/wixcopy/media/DSC_0014.jpg?raw=true">
+          <img class="gallery-small-pic gallery"
+            src="https://github.com/snickers-c/commercesurf/blob/wix/wixcopy/media/DSC_0026.jpg?raw=true">
+        </div>
+      </div>
+      <div class="gallery-right1">
+        <div class="gallery-lower-small">
+          <div class="gallery-2img-fix contentpic">
+            <img class="gallery-small-pic gallery"
+              src="https://github.com/snickers-c/commercesurf/blob/wix/wixcopy/media/DSC_0034.jpg?raw=true">
+            <div class="pictext">E-commerce lab</div>
+          </div>
+          <div class="gallery-2img-fix contentpic">
+            <img class="gallery-small-pic gallery"
+              src="https://github.com/snickers-c/commercesurf/blob/wix/wixcopy/media/DSC_0037.jpg?raw=true">
+            <div class="pictext">E-commerce lab</div>
+          </div>
+        </div>
+        <div class="gallery-upper-medium margintop10 contentpic">
+          <img class="gallery-medium-pic gallery"
+            src="https://github.com/snickers-c/commercesurf/blob/wix/wixcopy/media/DSC_0049.jpg?raw=true">
+        </div>
+      </div>
+    </div>
+    <div class="gallery-row2 margintop10">
+      <div class="gallery-left2">
+        <div class="gallery-lower-small contentpic">
+          <img class="gallery-smaller-pic gallery"
+            src="https://github.com/snickers-c/commercesurf/blob/wix/wixcopy/media/DSC_0078.jpg?raw=true">
+          <img class="gallery-smaller-pic gallery"
+            src="https://github.com/snickers-c/commercesurf/blob/wix/wixcopy/media/DSC_0078.jpg?raw=true">
+        </div>
+        <div class="gallery-upper-medium margintop10 contentpic">
+          <img class="gallery-mediumer-pic gallery"
+            src="https://github.com/snickers-c/commercesurf/blob/wix/wixcopy/media/DSC_0078.jpg?raw=true">
+        </div>
+      </div>
+      <div class="gallery-right2">
+        <div class="gallery-large contentpic">
+          <img class="gallery-large-pic gallery"
+            src="https://github.com/snickers-c/commercesurf/blob/wix/wixcopy/media/DSC_0078.jpg?raw=true">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="line-breaker"></div>
+</body>
+<footer>
+  <?php
+  include('./footer.php');
+  ?>
+</footer>
+</html>
