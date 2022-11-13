8c4de709de3474e287de9e5f16dc36b4ae6b07f5
RajmanCHesus <114088685+RajmanCHesus@users.noreply.github.com>
2022-11-13 11:21:35 +0100

    Update and rename obchodne-podmienky.html to obchodne-podmienky.php

---
+++ ecommerceLab/obchodne-podmienky.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Obchodné podmienky | E-commerce lab</title>
+  <?php
+  include('./head.php');
+  ?>
+  <!-- Roboto -->
+</head>
+<body>
+  <?php
+  include('./header.php');
+  ?>
+  <img class="background-img"
+    src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/Untitled%20design.png"
+    alt="white-sand">
+  <div class="page-template">
+    <section class="section-template">
+      <div class="business-terms-title">
+        OBCHODNÉ PODMIENKY
+      </div>
+      <p class="business-terms-text">
+        Pomyłka 404<br>
+        Strona w budowie
+      </p>
+    </section>
+  </div>
+  <div class="line-breaker"></div>
+</body>
+<footer>
+  <?php
+  include('./footer.php');
+  ?>
+</footer>
+</html>
