475f1b25320c4d9c4519cb34b4006e27fdfac2cc
RajmanCHesus <114088685+RajmanCHesus@users.noreply.github.com>
2022-11-13 11:19:50 +0100

    Update and rename kurzy.html to kurzy.php

---
+++ ecommerceLab/kurzy.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Kurzy | E-commerce lab</title>
+  <?php
+    include('./head.php');
+    ?>
+  <!-- Roboto -->
+</head>
+<body>
+  <?php
+  include('./header.php');
+  ?>
+  <img class="background-img"
+    src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/Untitled%20design.png"
+    alt="white-sand">
+  <div class="page-template">
+    <section class="section-template">
+      <div class="course-title">
+        Naše kurzy
+      </div>
+      <div class="course">
+        <div class="course-img-div">
+          <img class="course-img"
+            src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/Entrance.png">
+        </div>
+        <div class="course-text">
+          <p>
+            Zážitkový kurz
+          </p>
+          <p>
+            Ukončené
+          </p>
+          <button class="blue-button"><a class="course-a" href="kurz-template.php">Zobraziť kurz</a></button><br><br>
+        </div>
+      </div>
+      <div class="course">
+        <div class="course-img-div">
+          <img class="course-img"
+            src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/Entrance.png">
+        </div>
+        <div class="course-text">
+          <p>
+            Zážitkový kurz
+          </p>
+          <p>
+            Ukončené
+          </p>
+          <button class="blue-button"><a class="course-a" href="kurz-template.php">Zobraziť kurz</a></button><br><br>
+        </div>
+      </div>
+    </section>
+  </div>
+  <div class="line-breaker"></div>
+</body>
+<footer>
+  <?php
+  include('./footer.php');
+  ?>
+</footer>
+</html>
