6afb2b7d89886836cfce4668ddff634ae7baa166
RajmanCHesus <114088685+RajmanCHesus@users.noreply.github.com>
2022-11-13 11:21:02 +0100

    Update and rename o-nas.html to o-nas.php

---
+++ ecommerceLab/o-nas.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>O nás | E-commerce lab</title>
+  <?php
+  include('./head.php');
+  ?>
+  <!-- Roboto -->
+</head>
+<body>
+  <?php
+  include('./header.php');
+  ?>
+  <img class="background-img"
+    src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/Untitled%20design.png"
+    alt="white-sand">
+  <div class="page-template">
+    <section class="section-template">
+      <div>
+        <p class="about-us-title">O NÁS
+        <p>
+      </div>
+      <div class="about-us-flex">
+        <div class="about-us-text-1">
+          Spoločnosť Moitin s.r.o. sa od roku založenia 2013 zaoberá stratégiou vzdelávacieho systému v Slovenskej
+          republike s prepojením na zamestnávateľov a prenesením ich požiadaviek na stredné odborné školy. Ponúkame
+          kompletné poradenstvo, analýzu, strategické zacielenie pre školy, vzdelávacie inštitúcie a zamestnávateľov z
+          rôznych oblastí priemyslu. Našim cieľom je pomáhať vzdelávacím inštitúciám vedieť aktuálne zareagovať na
+          potreby trhu práce, pripraviť kvalitných a trhom požadovaných odborníkov pre efektívne fungovanie priemyslu.
+          Rovnako poskytujeme služby v oblasti IT projektov, projektov z EÚ fondov a ponúkame nové vízie, kde
+          implementujeme inovatívne prístupy s dôrazom na vysokú efektivitu a pridanú hodnotu.
+        </div>
+      </div>
+      <div class="about-us-flex">
+        <div class="about-us-text-2">
+          PhDr. Slavomír Terezka, riaditeľ Moitin s.r.o.<br>Špecialista v oblasti vzdelávacej politiky, strategický
+          konzultant pre IKT sektor a autor 1. študijného odboru na Slovensku pre IKT sektor „Informačné a sieťové
+          technológie“.
+        </div>
+      </div>
+      <div class="Slavo-class">
+        <img src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/terezka.png"
+          alt="Slavomír">
+      </div>
+    </section>
+  </div>
+  <div class="line-breaker"></div>
+</body>
+<footer>
+  <?php
+  include('./footer.php');
+  ?>
+</footer>
+</html>
