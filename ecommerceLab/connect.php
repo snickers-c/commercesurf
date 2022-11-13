0dd30259bda370f0d9dae0352616caa69ac39012
RajmanCHesus <114088685+RajmanCHesus@users.noreply.github.com>
2022-11-13 11:23:13 +0100

    Add files via upload

---
+++ ecommerceLab/connect.php
@@ -0,0 +1,12 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "automatkurz";
+
+
+    $conn = new mysqli($servername,$username,$password,$dbname);
+
+
+
+?>
---
+++ ecommerceLab/header.php
@@ -0,0 +1,16 @@
+<div class="header">
+  <div class="div-header-logo">
+    <a href="./index.php">
+      <img class="header-logo-lab" 
+        src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/ecommercelogo.png" >
+    </a>
+  </div>
+  <div class="header-links">
+    <a class="header-a" href="index.php">Domov</a>
+    <a class="header-a" href="kurzy.php">Kurzy</a>
+    <a class="header-a" href="aktuality.php">Aktuality</a>
+    <a class="header-a" href="galeria.php">Galéria</a>
+    <a class="header-a" href="o-nas.php">O nás</a>
+    <a class="header-a" href="kontakt.php">Kontakt</a>
+  </div>
+</div>
