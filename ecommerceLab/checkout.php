2f7603d6c365807ac22d11a8148874a2da67f05c
snickers-c <67205373+snickers-c@users.noreply.github.com>
2022-11-15 22:06:17 +0100

    Update checkout.php

--- ecommerceLab/checkout.php
+++ ecommerceLab/checkout.php
@@ -1,1 +1,82 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php include('./head.php') ?>
+  </head>
+  <body>
+    <?php include('./header.php') ?>
+    <img class="background-img"
+    src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/Untitled%20design.png"
+    alt="white-sand">
+    <div class="page-template">
+      <section class="section-template">
+        <form>
+          <div class="checkout-row">
+            <div class="checkout-left-section">
+              <div class="checkout-form">
+                <p class="checkout-section-title">Údaje účastníka kurzu</p>
+                <div class="underline100"></div>
+                  <input type="text" name="meno" placeholder="Meno"><br>
+                  <input type="text" name="priezvisko" placeholder="Priezvisko"><br>
+                  <input type="email" name="email" placeholder="E-mail"><br>
+                  <input type="text" name="telefon" placeholder="Telefónny kontakt"><br>
+                  <input type="text" name="ulica" placeholder="Ulica"><br>
+                  <input type="text" name="mesto" placeholder="Mesto"><br>
+                  <input type="text" name="psc" placeholder="PSČ"><br>
+                  <select name="mesto">
+                    <option value="sk">Slovenská republika</option>
+                    <option value="cz">Česká republika</option>
+                    <option value="ine">Íne</option>
+                  </select>
+              </div>
+            </div>
+            <div class="checkout-right-section">
+              <p class="checkout-section-title">Zhrnutie objednávky</p>
+              <div class="underline100"></div>
+              <div class="checkout-info">
+                <p>Krátky popis kurzu na jeden riadok by mohol idealne byť</p>
+                <img style="width:100px;height:100px;" src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/Entrance.png">
+                <p>datum</p>
+                <p>pocet volnych miest</p>
+                <p>cena</p>
+              </div>
+            </div>
+          </div>
+          <div class="checkout-row">
+            <div class="checkout-left-section">
+                <div class="checkout-form">
+                  <p class="checkout-section-title">Poznámka</p>
+                  <div class="underline100"></div>
+                    <input type="text" name="poznamka" placeholder="Povecte nám čo máte na mysli."><br>
+                </div>
+              </div>
+              <div class="checkout-right-section">
+                <p class="checkout-section-title">Platba</p>
+                <div class="underline100"></div>
+                <input type="radio" checked>
+                <label>Platba bankovým prevodom</label><br>
+                <p>
+                  PLATOBNÉ INFORMÁCIE: <br>
+                  Bankové spojenie: SLSP a.s. <br>
+                  Číslo účtu: 5124836961 / 0900 <br>
+                  IBAN: SK0709000000005124836961 <br>
+                  SWIFT: GIBASKBX <br>
 
+                  Fakturačné informácie: <br>
+                  Moitin s.r.o. <br>
+                  Miletičova 5B <br>
+                  821 08 Bratislava <br>
+                  Slovenská republika <br>
+                  IČO: 47349069 <br>
+                  IČ DPH: SK2023823131 <br>
+                </p>
+              </div>
+          </div>
+        </form>
+      </section>
+    </div>
+  </body>
+  <footer>
+    <?php include('./footer.php') ?>
+  </footer>
+</html>
