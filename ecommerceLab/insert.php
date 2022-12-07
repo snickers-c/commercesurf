89fba9a364738ade666dd0f78484a37f9c95d11b
RajmanCHesus <114088685+RajmanCHesus@users.noreply.github.com>
2022-12-07 18:33:41 +0100

    Create insert.php

---
+++ ecommerceLab/insert.php
@@ -0,0 +1,14 @@
+<?php
+    include_once('./connect.php');
+    $meno = $_POST["meno"];
+    $priezvisko = $_POST["priezvisko"];
+    $email = $_POST["email"];
+    $telefon = $_POST["telefon"];
+    $ulica = $_POST["ulica"];
+    $mesto = $_POST["mesto"];
+    $psc = $_POST["psc"];
+
+
+    $sql = "INSERT INTO zakaznici (meno , priezvisko , email , telefon, ulica , mesto , psc) VALUES ('$meno', '$priezvisko', '$email', '$telefon', '$ulica', '$mesto', '$psc');";
+mysqli_query($conn, $sql);
+?>
