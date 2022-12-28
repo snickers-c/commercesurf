61c1b47e0964fe0d19a2775f29d35dc54c6bf313
snickers-c <67205373+snickers-c@users.noreply.github.com>
2022-12-28 12:09:58 +0100

    Update aktuality.php

--- ecommerceLab/aktuality.php
+++ ecommerceLab/aktuality.php
@@ -10,30 +10,38 @@
   <?php
   include('./header.php');
   ?>
-  <img class="background-img"
-    src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/Untitled%20design.png"
-    alt="white-sand">
-  <div class="page-template">
-    <div class="news-flex">
-      <div class="news-container">
-        <p class="news-title">
-          Prázdninový e-commerce camp
-        </p>
-        <p class="news-autor">
-          Aktuality / xxxxxx
-        </p>
-        <p class="news-text">
-          Zabavíš sa a získaš cenné know-how zo sveta eshopov!<br><br>
-          Radi by sme Ti predstavili náš jedinečný Prázdninový e-commerce camp organizujúci Asociáciou spoločností IT
-          priemyslu s partnermi Acer,
-          SPŠE Hálova 16 a spoločnosťou Moitin ako prevádzkovateľa eshopu www.modelsnavigator.com
-        </p>
-        <a class="news-readmore" href="aktuality-template.php">
-          Čítajte viac
-        </a>
-      </div>
+    <img class="background-img"
+      src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/Untitled%20design.png"
+      alt="white-sand">
+    <div class="page-template">
+      <section class="section-template">
+        <?php
+          include('connect.php');
+          $result = ($conn->query("SELECT * FROM aktuality WHERE active = 1"));
+          $row_aktuality = [];
+          if ($result ->num_rows > 0)
+          {
+            $row_aktuality = $result ->fetch_all(MYSQLI_ASSOC);
+          }
+          if(!empty($row_aktuality))
+            foreach($row_aktuality as $rows)
+            {
+              echo '
+          <div class="news-container">
+            <p class="news-title">' . $rows['title'] . '</p>
+            <p class="news-autor">Aktuality / ' . $rows['autor'] . '</p>
+            <p class="news-text">' . $rows['text'] . '</p>
+            <a class="news-readmore" href="aktuality-template?ide=' . $rows['id'] . '">
+              Čítajte viac
+            </a>
+          </div>';
+            }
+          if(empty($row_aktuality)) {
+            echo '<div class="red-text">Momentálne pre Vás nemáme pripravené nič aktuálne.</div>';
+          }
+  ?>
+      </section>
     </div>
-  </div>
 </body>
 <footer>
   <?php
