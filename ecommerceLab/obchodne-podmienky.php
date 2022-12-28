981dd6f038c7f17220b27b91b9cb612f0de64e18
RajmanCHesus <114088685+RajmanCHesus@users.noreply.github.com>
2022-12-28 12:46:42 +0100

    Update obchodne-podmienky.php

--- ecommerceLab/obchodne-podmienky.php
+++ ecommerceLab/obchodne-podmienky.php
@@ -15,77 +15,116 @@
         alt="white-sand">
     <div class="page-template">
         <section class="section-template">
-            <div class="business-terms-title">
-                VŠEOBECNÉ OBCHODNÉ PODMIENKY
+            <div >
+                <div class="business-terms-title">
+                    VŠEOBECNÉ OBCHODNÉ PODMIENKY
+                </div>
+                <br>
+                <p class="business-terms-paragraph">1. Spoločnosť</p>
+                <p>ecommercelab.sk prevádzkuje spoločnosť Asit, o. z., Miletičová 5/B , 821 08 Bratislava, IČO:
+                    42266424, DIČ:
+                    2023655953. ecommercelab.sk je zameraný na vzdelávanie v obore ecommerce.</p>
+                <p class="business-terms-paragraph">2. Prihlásenie do kurzu</p>
+                <p>Na dokumentačné, propagačné a marketingové účely môže organizátor zo vzdelávacieho podujatia
+                    vyhotovovať a
+                    zverejňovať fotografie, prípadne obrazové resp. obrazovo-zvukové záznamy, ktoré môžu zachytávať aj
+                    podobu či
+                    prejavy jeho účastníkov. Účastníkom vzdelávacieho podujatia je fyzická osoba, ktorá na základe
+                    prihlášky
+                    zaslanej organizátorovi, sa zúčastňuje vzdelávacieho podujatia uskutočňovaného organizátorom.</p>
+                <p class="business-terms-paragraph">3. Objednávanie</p>
+                <p>Pri každom tovare je uvedená príslušná cena vrátane DPH 20%, stručná charakteristika kurzu a obrázok.
+                    Doba
+                    potrebná na vybavenie zadanej objednávky cez obchod je maximálne 24 hod. Táto doba je potrebná na
+                    spracovanie objednávky a my sa ju snažíme dodržať. Ak platíte prevodom na účet, táto doba začína
+                    plynúť až
+                    po poukázaní čiastky na náš účet.</p>
+                <p class="business-terms-paragraph">4. Platba</p>
+                <p>V našom E-shope môžete platiť len online bankovým prevodom. Objednávateľom je fyzická osoba alebo
+                    právnická
+                    osoba, ktorá zašle organizátorovi prihlášku na vzdelávacie podujatie. Objednávateľ môže ale nemusí
+                    byť
+                    totožný s osobou účastníka. Objednávateľ, ktorý nie je totožný s osobou účastníka spolu so zaslaním
+                    prihlášky oznámi organizátorovi aj meno a priezvisko, príp. ďalšie potrebné identifikačné údaje
+                    účastníka
+                    resp. účastníkov, ktorých na vzdelávacie podujatie prihlasuje. Spolu s prihláškou objednávateľ
+                    zasiela
+                    organizátorovi svoje fakturačné údaje za účelom vystavenia daňového dokladu.</p>
+                <p class="business-terms-paragraph">5. Zaradenie do skupín, trvanie, veľkosť skupín</p>
+                <p>Prihlášku objednávateľ zasiela elektronicky cez stránku ktorá sa uloží do databázy : ecommercelab.sk.
+                    Za
+                    zaslanie prihlášky na vzdelávacie podujatie sa považuje aj zaslanie oznámenia o záujme zúčastniť sa
+                    vzdelávacieho podujatia, ktoré objednávateľ vykoná prostredníctvom kontaktného formulára
+                    umiestneného na
+                    webovom sídle organizátora : ecommercelab.sk.</p>
+                <p class="business-terms-paragraph">6. Účasť na výučbe a vymeškané hodiny</p>
+                <p>Zaslaním prihlášky objednávateľ potvrdzuje, že sa oboznámil s týmito Všeobecnými zmluvnými
+                    podmienkami a
+                    súhlasí s nimi. Zaslaním prihlášky objednávateľ zároveň udeľuje organizátorovi súhlas s
+                    vyhotovovaním a
+                    zverejňovaním fotografií a obrazovej, resp. obrazovo – zvukovej dokumentácie zo vzdelávacieho
+                    podujatia na
+                    dokumentačné a marketingové účely, pričom objednávateľ si je vedomý toho, že takáto dokumentácia á
+                    môžu
+                    zaznamenávať aj podobizeň resp. prejavy osobnej povahy jednotlivých účastníkov vzdelávacieho
+                    podujatia.</p>
+                <p class="business-terms-paragraph">7. Prázdniny a voľná</p>
+                <p>Zaslaním prihlášky, dochádza medzi objednávateľom a organizátorom k vzniku predzmluvného vzťahu, na
+                    základe
+                    ktorého organizátor spracúva osobné údaje objednávateľa a/alebo účastníka vzdelávacieho podujatia v
+                    súlade
+                    so zákonom č. 18/2018 Z.z. o ochrane osobných údajov bez súhlasu. Zaslaním prihlášky a uhradením
+                    účastníckeho poplatku dochádza medzi objednávateľom a organizátorom k vzniku zmluvného vzťahu,
+                    pričom sa má
+                    za to, že organizátor a objednávateľ uzatvárajú dohodu podľa zákona č. 18/1996 Z.z. o cenách v znení
+                    zmien a
+                    doplnkov.</p>
+                <p class="business-terms-paragraph">8. Certifikáty a potvrdenia</p>
+                <p>Po uzavretí dohody v organizátor ďalej spracúva osobné údaje účastníka podľa zákona č.18/2018 Z.z. o
+                    ochrane
+                    osobných údajov v aktuálnom znení, pričom ide o spracúvanie osobných údajov bez súhlasu. Originál
+                    a/alebo
+                    rozmnoženiny materiálu obdržaného od organizátora vzdelávacieho podujatia, nie je účastníkovi
+                    dovolené ďalej
+                    rozširovať, scudzovať, postupovať ani poskytovať tretím osobám a ich používanie je určené výhradne
+                    na
+                    vlastné účely konkrétneho účastníka, ktorý sa daného vzdelávacieho podujatia zúčastnil.</p>
+                <p class="business-terms-paragraph">9. Podmienky pre stornovanie kurzu a vrátenie kurzovného</p>
+                <p>Účastník vzdelávacieho podujatia nie je oprávnený bez osobitného súhlasu organizátora a iných
+                    účastníkov
+                    vzdelávacieho podujatia vyhotovovať a/alebo zverejňovať zo vzdelávacieho podujatia fotografie,
+                    obrazové
+                    alebo obrazovo-zvukové záznamy. Organizátor si vyhradzuje právo zrušiť vzdelávacie podujatie, a to
+                    aj bez
+                    udania dôvodu. V takomto prípade je organizátor povinný objednávateľa o zrušení vzdelávacieho
+                    podujatia
+                    vopred informovať a vrátiť objednávateľovi zaplatený účastnícky poplatok. Reklamácie našich kurzov
+                    neposkytujeme.</p>
+                <p class="business-terms-paragraph">10. Zodpovednosť</p>
+                <p>ecommercelab.sk nezodpovedá za ujmu na zdraví a majetku účastníkov. O účasti účastníka na vzdelávacom
+                    podujatí vydá organizátor účastníkovi potvrdenie o účasti, resp. osvedčenie o účasti, resp.
+                    certifikát a to
+                    v závislosti od typu vzdelávacieho podujatia, ktorého sa účastník zúčastnil. V prípade neúčasti
+                    prihláseného
+                    účastníka na vzdelávacom podujatí sa účastnícky poplatok nevracia.</p>
+                <p class="business-terms-paragraph">11. Ochrana osobných údajov</p>
+                <p>Prevádzkovateľ pri spracovaní osobných údajov postupuje v súlade s nasledujúcimi právnymi predpismi:
+                    • Zákonč. 18/2018 Z. z. o ochrane osobných údajov
+                    • Nariadenie Európskeho parlamentu a Rady (EÚ) 2016/679 (ďalej
+                    len "GDPR") Svojím zakúpením nášho kurzu súhlasíte s tým, že prevádzkovateľ domény ecommercelab.sk
+                    bude v
+                    súlade s príslušnými všeobecne platnými právnymi predpismi spracovávať a uchovávať všetky informácie
+                    a
+                    údaje, ktoré ste nám poskytli pri platbe. Tento súhlas nám udeľujete na dobu neurčitú. Vaše osobné
+                    údaje sú
+                    uchovávané v zabezpečenej databáze a budú použité len pre potreby nášho obchodovania s vami. Zároveň
+                    predpokladáme, že pri vašom nákupe sme od vás získali prehlásenie, že ste s týmto poučený o svojom
+                    práve na
+                    prístup k vašim osobným údajom, práve na ich dopravu alebo ich vymazanie z databázy ako aj o ďalších
+                    vašich
+                    právach.<br><br>
+                    <b>Tieto všeobecné podmienky sú platné od 15.12.2022.</b></p>
             </div>
-            <br><p class="business-terms-paragraph">1. Spoločnosť</p>
-            <p>ecommercelab.sk prevádzkuje spoločnosť Asit, o. z., Miletičová 5/B , 821 08 Bratislava, IČO: 42266424, DIČ:
-            2023655953. ecommercelab.sk je zameraný na vzdelávanie v obore ecommerce.</p>
-            <p class="business-terms-paragraph">2. Prihlásenie do kurzu</p>
-            <p>Na dokumentačné, propagačné a marketingové účely môže organizátor zo vzdelávacieho podujatia vyhotovovať a
-            zverejňovať fotografie, prípadne obrazové resp. obrazovo-zvukové záznamy, ktoré môžu zachytávať aj podobu či
-            prejavy jeho účastníkov. Účastníkom vzdelávacieho podujatia je fyzická osoba, ktorá na základe prihlášky
-            zaslanej organizátorovi, sa zúčastňuje vzdelávacieho podujatia uskutočňovaného organizátorom.</p>
-            <p class="business-terms-paragraph">3. Objednávanie</p>
-            <p>Pri každom tovare je uvedená príslušná cena vrátane DPH 20%, stručná charakteristika kurzu a obrázok. Doba
-            potrebná na vybavenie zadanej objednávky cez obchod je maximálne 24 hod. Táto doba je potrebná na
-            spracovanie objednávky a my sa ju snažíme dodržať. Ak platíte prevodom na účet, táto doba začína plynúť až
-            po poukázaní čiastky na náš účet.</p>
-            <p class="business-terms-paragraph">4. Platba</p>
-            <p>V našom E-shope môžete platiť len online bankovým prevodom. Objednávateľom je fyzická osoba alebo právnická
-            osoba, ktorá zašle organizátorovi prihlášku na vzdelávacie podujatie. Objednávateľ môže ale nemusí byť
-            totožný s osobou účastníka. Objednávateľ, ktorý nie je totožný s osobou účastníka spolu so zaslaním
-            prihlášky oznámi organizátorovi aj meno a priezvisko, príp. ďalšie potrebné identifikačné údaje účastníka
-            resp. účastníkov, ktorých na vzdelávacie podujatie prihlasuje. Spolu s prihláškou objednávateľ zasiela
-            organizátorovi svoje fakturačné údaje za účelom vystavenia daňového dokladu.</p>
-            <p class="business-terms-paragraph">5. Zaradenie do skupín, trvanie, veľkosť skupín</p>
-            <p>Prihlášku objednávateľ zasiela elektronicky cez stránku ktorá sa uloží do databázy : ecommercelab.sk. Za
-            zaslanie prihlášky na vzdelávacie podujatie sa považuje aj zaslanie oznámenia o záujme zúčastniť sa
-            vzdelávacieho podujatia, ktoré objednávateľ vykoná prostredníctvom kontaktného formulára umiestneného na
-            webovom sídle organizátora : ecommercelab.sk.</p>
-            <p class="business-terms-paragraph">6. Účasť na výučbe a vymeškané hodiny</p>
-            <p>Zaslaním prihlášky objednávateľ potvrdzuje, že sa oboznámil s týmito Všeobecnými zmluvnými podmienkami a
-            súhlasí s nimi. Zaslaním prihlášky objednávateľ zároveň udeľuje organizátorovi súhlas s vyhotovovaním a
-            zverejňovaním fotografií a obrazovej, resp. obrazovo – zvukovej dokumentácie zo vzdelávacieho podujatia na
-            dokumentačné a marketingové účely, pričom objednávateľ si je vedomý toho, že takáto dokumentácia á môžu
-            zaznamenávať aj podobizeň resp. prejavy osobnej povahy jednotlivých účastníkov vzdelávacieho podujatia.</p>
-            <p class="business-terms-paragraph">7. Prázdniny a voľná</p>
-            <p>Zaslaním prihlášky, dochádza medzi objednávateľom a organizátorom k vzniku predzmluvného vzťahu, na základe
-            ktorého organizátor spracúva osobné údaje objednávateľa a/alebo účastníka vzdelávacieho podujatia v súlade
-            so zákonom č. 18/2018 Z.z. o ochrane osobných údajov bez súhlasu. Zaslaním prihlášky a uhradením
-            účastníckeho poplatku dochádza medzi objednávateľom a organizátorom k vzniku zmluvného vzťahu, pričom sa má
-            za to, že organizátor a objednávateľ uzatvárajú dohodu podľa zákona č. 18/1996 Z.z. o cenách v znení zmien a
-            doplnkov.</p>
-            <p class="business-terms-paragraph">8. Certifikáty a potvrdenia</p>
-            <p>Po uzavretí dohody v organizátor ďalej spracúva osobné údaje účastníka podľa zákona č.18/2018 Z.z. o ochrane
-            osobných údajov v aktuálnom znení, pričom ide o spracúvanie osobných údajov bez súhlasu. Originál a/alebo
-            rozmnoženiny materiálu obdržaného od organizátora vzdelávacieho podujatia, nie je účastníkovi dovolené ďalej
-            rozširovať, scudzovať, postupovať ani poskytovať tretím osobám a ich používanie je určené výhradne na
-            vlastné účely konkrétneho účastníka, ktorý sa daného vzdelávacieho podujatia zúčastnil.</p>
-            <p class="business-terms-paragraph">9. Podmienky pre stornovanie kurzu a vrátenie kurzovného</p>
-            <p>Účastník vzdelávacieho podujatia nie je oprávnený bez osobitného súhlasu organizátora a iných účastníkov
-            vzdelávacieho podujatia vyhotovovať a/alebo zverejňovať zo vzdelávacieho podujatia fotografie, obrazové
-            alebo obrazovo-zvukové záznamy. Organizátor si vyhradzuje právo zrušiť vzdelávacie podujatie, a to aj bez
-            udania dôvodu. V takomto prípade je organizátor povinný objednávateľa o zrušení vzdelávacieho podujatia
-            vopred informovať a vrátiť objednávateľovi zaplatený účastnícky poplatok. Reklamácie našich kurzov
-            neposkytujeme.</p>
-            <p class="business-terms-paragraph">10. Zodpovednosť</p>
-            <p>ecommercelab.sk nezodpovedá za ujmu na zdraví a majetku účastníkov. O účasti účastníka na vzdelávacom
-            podujatí vydá organizátor účastníkovi potvrdenie o účasti, resp. osvedčenie o účasti, resp. certifikát a to
-            v závislosti od typu vzdelávacieho podujatia, ktorého sa účastník zúčastnil. V prípade neúčasti prihláseného
-            účastníka na vzdelávacom podujatí sa účastnícky poplatok nevracia.</p>
-            <p class="business-terms-paragraph">11. Ochrana osobných údajov</p>
-            <p>Prevádzkovateľ pri spracovaní osobných údajov postupuje v súlade s nasledujúcimi právnymi predpismi: 
-              • Zákonč. 18/2018 Z. z. o ochrane osobných údajov 
-              • Nariadenie Európskeho parlamentu a Rady (EÚ) 2016/679 (ďalej
-            len "GDPR") Svojím zakúpením nášho kurzu súhlasíte s tým, že prevádzkovateľ domény ecommercelab.sk bude v
-            súlade s príslušnými všeobecne platnými právnymi predpismi spracovávať a uchovávať všetky informácie a
-            údaje, ktoré ste nám poskytli pri platbe. Tento súhlas nám udeľujete na dobu neurčitú. Vaše osobné údaje sú
-            uchovávané v zabezpečenej databáze a budú použité len pre potreby nášho obchodovania s vami. Zároveň
-            predpokladáme, že pri vašom nákupe sme od vás získali prehlásenie, že ste s týmto poučený o svojom práve na
-            prístup k vašim osobným údajom, práve na ich dopravu alebo ich vymazanie z databázy ako aj o ďalších vašich
-            právach.<br><br>
-            Tieto všeobecné podmienky sú platné od 15.12.2022.</p>
         </section>
     </div>
 </body>
