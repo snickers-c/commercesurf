25d1e0afe39545c3281a74c92d811ed403d31d91
RajmanCHesus <114088685+RajmanCHesus@users.noreply.github.com>
2022-12-28 13:42:55 +0100

    Update kontakt.php

--- ecommerceLab/kontakt.php
+++ ecommerceLab/kontakt.php
@@ -1,109 +1,111 @@
 <!DOCTYPE html>
 <head>
-  <title>Kontakt | E-commerce lab</title>
-  <?php
+    <title>Kontakt | E-commerce lab</title>
+    <?php
   include('./head.php');
   ?>
-  <!-- Roboto -->
+    <!-- Roboto -->
 </head>
 <body>
-  <?php
+    <?php
   include('./header.php');
   ?>
-  <img class="background-img"
-    src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/Untitled%20design.png"
-    alt="white-sand">
-  <div class="page-template">
-    <div class="contact-title">
-      Kontakt
-    </div>
-    <div class="contact-container">
-      <div class="contact-text-container1">
-        <div class="contact-text1">
-          <p class="contact-heading">
-            Kontaktujte nás:
-          </p>
-          <p>
-            Mobil: 
-            <br>+421948068744<br> +421905639141
-          </p>
-          <p>
-            Informácie: info.ecommercelab@gmail.com
-          </p>
+    <img class="background-img"
+        src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/Untitled%20design.png"
+        alt="white-sand">
+    <div class="page-template">
+        <div class="section-template">
+            <div class="contact-title">
+                Kontakt
+            </div>
+            <div class="contact-container">
+                <div class="contact-text-container1">
+                    <div class="contact-text1">
+                        <p class="contact-heading">
+                            Kontaktujte nás:
+                        </p>
+                        <p>
+                            Mobil:
+                            <br>+421948068744<br> +421905639141
+                        </p>
+                        <p>
+                            Informácie: info.ecommercelab@gmail.com
+                        </p>
+                    </div>
+                    <div class="contact-text2">
+                        <p class="contact-heading">
+                            Adresa:
+                        </p>
+                        <p>
+                            Hálova 16, 851 01 Bratislava (budova SPŠ elektrotechnickej, bočný vchod)
+                        </p>
+                    </div>
+                </div>
+                <div class="contact-text-container2">
+                    <div class="contact-text3">
+                        <p class="contact-heading">
+                            Platobné informácie:
+                        </p>
+                        <p>
+                            Bankové spojenie: SLSP a.s.<br>
+                            Číslo účtu: 5124836961 / 0900<br>
+                            IBAN: SK0709000000005124836961<br>
+                            SWIFT: GIBASKBX
+                        </p>
+                        <p>
+                            Fakturačné informácie:<br>
+                            Moitin s.r.o.<br>
+                            Miletičova 5B<br>
+                            821 08 Bratislava<br>
+                            Slovenská republika<br>
+                            IČO: 47349069<br>
+                            IČ DPH: SK2023823131
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="img1">
+                <div class="img-margine">
+                    <img class="img-frame1"
+                        src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/Entrance.png"
+                        alt="Entrance" width="430">
+                    <img class="img-frame2"
+                        src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/LAB.png"
+                        alt="LAB" width="430">
+                </div>
+            </div>
+            <div class="map" class="mapouter">
+                <div class="gmap_canvas">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1119.8493390528681!2d17.104232292766657!3d48.12054370298708!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476c899fd4733b61%3A0xe61e45d972240830!2zSMOhbG92YSA5OTMvMTYsIDg1MSAwMSBQZXRyxb5hbGth!5e0!3m2!1ssk!2ssk!4v1669746022435!5m2!1ssk!2ssk"
+                        width="850" height="500" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <style>
+                    .mapouter {
+                        position: relative;
+                        text-align: center;
+                        height: 500px;
+                        width: 850px;
+                    }
+                    </style>
+                    <a href="https://www.embedgooglemap.net"></a>
+                    <style>
+                    .gmap_canvas {
+                        overflow: hidden;
+                        background: none !important;
+                        height: 500px;
+                        width: 850px;
+                        margin-top: 100px;
+                        margin-bottom: 100px;
+                    }
+                    </style>
+                </div>
+            </div>
         </div>
-        <div class="contact-text2">
-          <p class="contact-heading">
-            Adresa:
-          </p>
-          <p>
-            Hálova 16, 851 01 Bratislava (budova SPŠ elektrotechnickej, bočný vchod)
-          </p>
-        </div>
-      </div>
-      <div class="contact-text-container2">
-        <div class="contact-text3">
-          <p class="contact-heading">
-            Platobné informácie:
-          </p>
-          <p>
-            Bankové spojenie: SLSP a.s.<br>
-            Číslo účtu: 5124836961 / 0900<br>
-            IBAN: SK0709000000005124836961<br>
-            SWIFT: GIBASKBX
-          </p>
-          <p>
-            Fakturačné informácie:<br>
-            Moitin s.r.o.<br>
-            Miletičova 5B<br>
-            821 08 Bratislava<br>
-            Slovenská republika<br>
-            IČO: 47349069<br>
-            IČ DPH: SK2023823131
-          </p>
-        </div>
-      </div>
     </div>
-    <div class="img1">
-      <div class="img-margine">
-        <img class="img-frame1"
-          src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/Entrance.png"
-          alt="Entrance" width="430">
-        <img class="img-frame2"
-          src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/LAB.png" alt="LAB"
-          width="430">
-      </div>
-    </div>
-    <div class="map" class="mapouter">
-      <div class="gmap_canvas">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1119.8493390528681!2d17.104232292766657!3d48.12054370298708!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476c899fd4733b61%3A0xe61e45d972240830!2zSMOhbG92YSA5OTMvMTYsIDg1MSAwMSBQZXRyxb5hbGth!5e0!3m2!1ssk!2ssk!4v1669746022435!5m2!1ssk!2ssk"
-          width="850" height="500" style="border:0;" allowfullscreen="" loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"></iframe>
-        <style>
-          .mapouter {
-            position: relative;
-            text-align: center;
-            height: 500px;
-            width: 850px;
-          }
-        </style>
-        <a href="https://www.embedgooglemap.net"></a>
-        <style>
-          .gmap_canvas {
-            overflow: hidden;
-            background: none !important;
-            height: 500px;
-            width: 850px;
-            margin-top: 100px;
-            margin-bottom: 100px;
-          }
-        </style>
-      </div>
-    </div>
-  </div>
 </body>
 <footer>
-  <?php
+    <?php
   include('./footer.php');
   ?>
 </footer>
