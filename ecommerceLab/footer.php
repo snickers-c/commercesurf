156f78ab93e239e5480308b2fc91fedc47bbe646
snickers-c <67205373+snickers-c@users.noreply.github.com>
2022-12-28 11:33:31 +0100

    Update footer.php

--- ecommerceLab/footer.php
+++ ecommerceLab/footer.php
@@ -1,59 +1,58 @@
-<footer>
-  <div class="footer-container">
-    <div class="footer-section">
-      <p class="footer-nadpis">KONTAKT</p>
-      <ul class="footer-kontakt-ul">
-        <li>
-          <p class="footer-kontakt-p"><span class="footer-bold">Adresa:</span> Hálova 16, 851 01 Bratislava<br>(budova SPŠ elektrotechnickej,
-            bočný vchod)</p>
-        </li>
-        <li>
-          <div class="p-flex"><span class="footer-bold">Telefón:</span>
-            <div class="footer-numbers">+421 948 068 744<br>+421 905 639 141</div>
-          </div>
-        </li>
-        <li>
-          <p class="footer-kontakt-p"><span class="footer-bold">Mail:</span> info@modelsnavigator.com</p>
-        </li>
-      </ul>
+
+<div class="footer-container">
+  <div class="footer-section">
+    <p class="footer-nadpis">KONTAKT</p>
+    <ul class="footer-kontakt-ul">
+      <li>
+        <p class="footer-kontakt-p"><span class="footer-bold">Adresa:</span> Hálova 16, 851 01 Bratislava<br>(budova SPŠ elektrotechnickej,
+          bočný vchod)</p>
+      </li>
+      <li>
+        <div class="p-flex"><span class="footer-bold">Telefón:</span>
+          <div class="footer-numbers">+421 948 068 744<br>+421 905 639 141</div>
+        </div>
+      </li>
+      <li>
+        <p class="footer-kontakt-p"><span class="footer-bold">Mail:</span> info.ecommercelab@gmail.com</p>
+      </li>
+    </ul>
+  </div>
+  <div class="footer-section">
+    <p class="footer-nadpis">INFORMÁCIE</p>
+    <a href="o-nas">
+      <p class="footer-info-links">O NÁS</p>
+    </a>
+    <a href="kontakt">
+      <p class="footer-info-links">KONTAKT</p>
+    </a>
+    <a href="aktuality">
+      <p class="footer-info-links">AKTUALITY</p>
+    </a>
+    <a href="obchodne-podmienky">
+      <p class="footer-info-links">VŠEOBECNÉ OBCHODNÉ PODMIENKY</p>
+    </a>
+  </div>
+  <div class="footer-section">
+    <p class="footer-nadpis">SOCIÁLNE SIETE</p>
+    <div class="footer-column-logo">
+      <a href="https://www.instagram.com/ecommercelab_ba/" target="_blank">
+        <img class="footer-logo"
+          src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/instagram.png">
+      </a>
+      <a href="https://www.facebook.com/EcommerceLABba" target="_blank">
+        <img class="footer-logo"
+          src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/Untitled%20design%20(1).png">
+      </a>
     </div>
-    <div class="footer-section">
-      <p class="footer-nadpis">INFORMÁCIE</p>
-      <a href="o-nas.php">
-        <p class="footer-info-links">O NÁS</p>
+    <div class="footer-column-logo">
+      <a href="https://www.youtube.com/channel/UCCrktgYViIzfXq6DkNZB5Lg" target="_blank">
+        <img class="footer-logo"
+          src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/youtube.png">
       </a>
-      <a href="kontakt.php">
-        <p class="footer-info-links">KONTAKT</p>
+      <a href="https://www.tiktok.com/@ecommercelab" target="_blank">
+        <img class="footer-logo"
+          src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/tiktok.png">
       </a>
-      <a href="aktuality.php">
-        <p class="footer-info-links">AKTUALITY</p>
-      </a>
-      <a href="obchodne-podmienky.php">
-        <p class="footer-info-links">VŠEOBECNÉ OBCHODNÉ PODMIENKY</p>
-      </a>
-    </div>
-    <div class="footer-section">
-      <p class="footer-nadpis">SOCIÁLNE SIETE</p>
-      <div class="footer-column-logo">
-        <a href="https://www.instagram.com/ecommercelab_ba/" target="_blank">
-          <img class="footer-logo"
-            src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/instagram.png">
-        </a>
-        <a href="https://www.facebook.com/EcommerceLABba" target="_blank">
-          <img class="footer-logo"
-            src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/Untitled%20design%20(1).png">
-        </a>
-      </div>
-      <div class="footer-column-logo">
-        <a href="https://www.youtube.com/channel/UCCrktgYViIzfXq6DkNZB5Lg" target="_blank">
-          <img class="footer-logo"
-            src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/youtube.png">
-        </a>
-        <a href="https://www.tiktok.com/@ecommercelab" target="_blank">
-          <img class="footer-logo"
-            src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/tiktok.png">
-        </a>
-      </div>
     </div>
   </div>
-</footer>
+</div>
