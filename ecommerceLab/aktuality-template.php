9b1d92a516d844098ff1fa836a3951bbaa60fad3
snickers-c <67205373+snickers-c@users.noreply.github.com>
2022-12-28 12:10:16 +0100

    Update aktuality-template.php

--- ecommerceLab/aktuality-template.php
+++ ecommerceLab/aktuality-template.php
@@ -1,9 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>* | E-commerce lab</title>
   <?php
+  include('connect.php');
   include('./head.php');
+  $url = $_SERVER['REQUEST_URI'];
+  $url_components = parse_url($url);
+  parse_str($url_components['query'], $params);
+  $result = ($conn->query("SELECT * FROM aktuality_templates WHERE id = $params[ide]"));
+            $row_aktuality = [];
+            if ($result ->num_rows > 0)
+            {
+              $row_aktuality = $result ->fetch_all(MYSQLI_ASSOC);
+            }
+            if(!empty($row_aktuality))
+              foreach($row_aktuality as $rows)
+              {
+                echo '<title>' . $rows['title'] . ' | E-commerce lab</title>';
+              }
   ?>
 </head>
 <?php
@@ -15,54 +29,21 @@ include('./header.php');
     alt="white-sand">
   <div class="page-template">
     <section class="section-template">
-      <div class="news-template-title">
-        Prázdninový e-commerce camp
-      </div>
-      <img class="news-template-img"
-        src="https://raw.githubusercontent.com/snickers-c/commercesurf/main/ecommerceLab/pictures/Entrance.png">
-      <p class="news-template-text">
-        Zabavíš sa a získaš cenné know-how zo sveta eshopov!<br>
-        Kapacita: 10 miest<br>
-        Prednostne pre žiakov: SPŠE Hálova 16<br>
-        Radi by sme Ti predstavili náš jedinečný Prázdninový e-commerce camp organizujúci Asociáciou spoločností IT
-        priemyslu s partnermi Acer, SPŠE Hálova 16 a spoločnosťou Moitin ako prevádzkovateľa eshopu
-        www.modelsnavigator.com<br>
-        Od nového školského roku 2021/2022 na SPŠE Hálova 16 budú mať žiaci možnosť zapojiť sa do duálneho vzdelávania
-        čo znamená prípravu na povolanie priamo za účasti odborníkov z praxe v jedinečnom prvom certifikovanom
-        e-commerce labe, kde vyučovanie bude prebiehať praktickou formou priamo vo firme. Tento segment je veľmi
-        žiadaný, aktuálne tam je AAA – Absolútna Absencia Absolventov. Medzi významných hráčov v elektronickom obchode
-        patria zamestnávatelia ako Ebay, Amazon, Alza a iné.<br>
-        Preto sme sa rozhodli dať jedinečnú šancu vopred zažiť čo je to svet elektronického obchodu v našom Prázdninovom
-        e-commerce campe.<br>
-        Ako to celé bude fungovať?<br>
-        Kedy? 12. – 16. júla 2021 (Pondelok až piatok od 8:00 do 16:00 hod.)<br>
-        Kde? Prevádzkové priestory eshopu Modelsnavigator v budove školy SPŠE Hálova 16.<br>
-        Cena 120,- €<br>
-        Obsah?<br><br>
-        1. deň<br>
-        shadowing, budeme Ťa sprevádzať reálnym svetom e-commerce počas celého dňa v našom eshope<br><br>
-        2. deň<br>
-        ukážeme Ti databázy, ich reálne fungovanie a počas dňa si môžeš reálne zarobiť<br><br>
-        3. deň<br>
-        naučíme Ťa produktovú fotografiu a zároveň najlepšia fotka bude odmenená<br><br>
-        4. deň<br>
-        deň plný kreativity pri customizácii produktu<br><br>
-        5. deň<br>
-        vytvoríš vlastný nápad, projekt a odprezentuješ sa<br><br>
-        Ak Ťa to zaujalo a chceš zažiť naplno svet e-commerce klikni tu pre registráciu:
-        https://forms.gle/JmNccT8ZH7t4eZrY9<br>
-        Ďalšie informácie:<br>
-        – obed zabezpečený<br>
-        – pitný režim zabezpečený<br>
-        – hygienické podmienky zabezpečené<br>
-        Tešíme sa na Tvoju účasť!<br>
-        Tím Modelsnavigator.com<br>
-        Bankové spojenie: SLSP a.s.<br>
-        Čislo učtu: 5124836961 / 0900<br>
-        IBAN: SK0709000000005124836961<br>
-        SWIFT: GIBASKBX<br>
-        Do poznámky uveďte meno a priezvisko žiaka.<br>
-      </p>
+      <?php
+            if(!empty($row_aktuality))
+              foreach($row_aktuality as $rows)
+              {
+                echo '
+          <div class="news-template-title">
+            ' . $rows['title'] . '
+          </div>
+          <div class="news-template-text-div">
+            <img class="news-template-img"
+              src="' . $rows['img'] . '">
+            <p class="news-template-text">' . $rows['text'] . '</p>
+          </div>';
+              }
+      ?>
     </section>
   </div>
 </body>
