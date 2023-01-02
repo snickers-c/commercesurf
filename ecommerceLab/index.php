454ff525c8ef6e680220c6ec35c5f0b37657b564
RajmanCHesus <114088685+RajmanCHesus@users.noreply.github.com>
2023-01-02 20:56:56 +0100

    Update index.php

--- ecommerceLab/index.php
+++ ecommerceLab/index.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Domov | E-commerce lab</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php
   include('./head.php');
   ?>
 </head>
+
 <body>
     <!-- horné menu -->
     <?php
@@ -40,7 +43,7 @@
         src=" ' . $rows['img']. ' "
         class="slider-img">
       <div class="text">' . $rows['title'] . '</div>
-      <a href="kurz-template?ide=' . $rows['id'] . '">
+      <a class="index-o-nas-button-viac-center" href="kurz?ide=' . $rows['id'] . '">
         <button class="find-more-slider-button">Zistiť viac</button>
       </a>
     </div>';
@@ -117,10 +120,85 @@
             </div>
         </div>
     </div>
+    <!-- <dialog open class="modal" id="modal" style="width: 60%;background-color: rgba(45, 175, 227, 1) ;border-radius: 10px; display: none;"> -->
+<dialog open id="modulo" style="width: 60%;background-color: rgba(45, 175, 227, 1) ;border: 1px; border-radius: 10px; display: none; position: fixed; z-index: 1002; bottom: 0;">
+    <div style="color: white; font-weight: bold; font-size: 18px;">
+        <div id="closeIcon" style="display: none;">
+        </div>
+        <div class="title-wrap">
+            <h4 style=" margin: 0; padding: 20px 10px 20px 10px; font-size: 22px;">Cookie Consent</h4>
+        </div>
+        <div style=" padding-left: 20px; ">
+            <div class="msg-wrap">
+                <p style="margin: 0; padding-bottom: 20px;">This website uses cookies or similar technologies, to enhance your browsing experience
+                    and provide
+                    personalized recommendations. By continuing to use our website, you agree to our <a
+                        style="color:#115cfa" href="/ecommerceLab/obchodne-podmienky.php">Privacy Policy</a></p>
+                <div  style="padding-bottom: 10px;">
+                    <button class="blue-button" style="background-color: white; color: rgba(45, 175, 227, 1); font-weight: bold ; border-radius: 5px; " onclick="acceptCookieConsent();">Accept</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</dialog>
+<!-- </dialog> -->
+<script>
+
+
+
+// Create cookie
+    function setCookie(cname, cvalue, exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        let expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+    
+    // Delete cookie
+    function deleteCookie(cname) {
+        const d = new Date();
+        d.setTime(d.getTime() + (24*60*60*1000));
+        let expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=;" + expires + ";path=/";
+    }
+    
+    // Read cookie
+    function getCookie(cname) {
+        let name = cname + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for(let i = 0; i <ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+    
+    // Set cookie consent
+    function acceptCookieConsent(){
+        deleteCookie('user_cookie_consent');
+        setCookie('user_cookie_consent', 1, 30);
+        document.getElementById("modulo").style.display = "none";
+    }
+    
+    
+    let cookie_consent = getCookie("user_cookie_consent");
+    if(cookie_consent != ""){
+        document.getElementById("modulo").style.display = "none";
+    }else{
+        document.getElementById("modulo").style.display = "block";
+    }</script>
 </body>
 <footer>
     <?php
   include('./footer.php');
   ?>
 </footer>
+
 </html>
